6ea862b25b2ec007818af5b79bc4c4e743adc96c
Saikrishna-Akhil <124485100+Saikrishna-Akhil@users.noreply.github.com>
2025-11-05 13:18:40 +0530

    new

--- src/main/java/com/example/AgriTechLogin/Security/OAuth2UserServiceImpl.java
+++ src/main/java/com/example/AgriTechLogin/Security/OAuth2UserServiceImpl.java
@@ -1,35 +1,28 @@
-package com.example.AgriTechLogin.Security;
-
-import com.example.AgriTechLogin.model.User;
-import com.example.AgriTechLogin.repository.UserRepository;
-
+package com.example.Agri.L.P.BE.security;
+import com.example.Agri.L.P.BE.model.User;
+import com.example.Agri.L.P.BE.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
 @Service
 @RequiredArgsConstructor
 public class OAuth2UserServiceImpl extends DefaultOAuth2UserService {
-
     private final UserRepository userRepository;
-
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         String email = oAuth2User.getAttribute("email");
-
         userRepository.findByEmail(email).orElseGet(() -> {
             User newUser = User.builder()
                     .name(oAuth2User.getAttribute("name"))
                     .email(email)
-                    .provider(userRequest.getClientRegistration().getRegistrationId()) // google/microsoft
+                    .provider(userRequest.getClientRegistration().getRegistrationId()) 
                     .build();
             userRepository.save(newUser);
             return newUser;
         });
-
         return new CustomOAuth2User(oAuth2User);
     }
 }
