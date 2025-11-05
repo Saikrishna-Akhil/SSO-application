f3600935ecc8123e5c657f74e0845a9ae38e0a0f
Saikrishna-Akhil <124485100+Saikrishna-Akhil@users.noreply.github.com>
2025-11-05 13:24:35 +0530

    Rename AgriTechLoginApplication to AgriFarmApplicationnew

--- src/main/java/com/example/AgriTechLogin/AgriTechLoginApplication.java
+++ src/main/java/com/example/AgriTechLogin/AgriTechLoginApplication.java
@@ -1,15 +1,11 @@
-package com.example.AgriTechLogin;
-
+package com.example.Agri.L.P.BE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.sql.SQLOutput;
 @SpringBootApplication
-public class AgriTechLoginApplication {
-
+public class AgriFarmApplication {
 	public static void main(String[] args) {
-
-		SpringApplication.run(AgriTechLoginApplication.class, args);
-		System.out.println("App Running....");
+		SpringApplication.run(AgriFarmApplication.class, args);
+		System.out.println("Running.......");
 	}
-
 }
